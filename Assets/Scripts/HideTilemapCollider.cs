46f713ff204b4b9f120df24da597fe78f5a7af71
Nixon Silva <nixonmoreira42@gmail.com>
2020-09-08 19:02:19 -0300

    Isometric Template
    Implemented the basics of an isometric map game. Nothing 'new' was developed.

---
+++ Assets/Scripts/HideTilemapCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class HideTilemapCollider : MonoBehaviour
+{
+
+    private TilemapRenderer tilemap;
+
+    private void Awake ()
+    {
+        tilemap = GetComponent<TilemapRenderer>();
+    }
+
+    private void Start ()
+    {
+        tilemap.enabled = false;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private float moveH, moveV;
+    [SerializeField] private float moveSpeed = 2.0f;
+
+    void Awake ()
+    {
+        rb = GetComponent<Rigidbody2D>();    
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate ()
+    {
+        rb.velocity = new Vector2(moveH, moveV);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveH = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        moveV = Input.GetAxisRaw("Vertical") * moveSpeed;
+    }
+}
